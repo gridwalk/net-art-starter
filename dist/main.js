a433545075014ec8720162b5b8051d2f8ea311dd
Donald Hanson <don@gridwalk.net>
2017-06-12 17:37:15 -0700

    some example event listeners

--- dist/main.js
+++ dist/main.js
@@ -36,3 +36,16 @@ function chanceExample() {
     // 50% chance to do something
   }
 }
+
+
+document.addEventListener('keydown',function(e){
+  console.log(e.which,'pressed')
+})
+
+document.addEventListener('mousedown',function(e){
+  console.log('clicked')
+})
+
+document.addEventListener('mousemove',function(e){
+  // console.log(e)
+})
